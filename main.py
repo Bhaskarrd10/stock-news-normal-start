dc7119ba35370991e26d776ba994b4b8efa93e03
bhaskarreddy <bhaskrrd10@gmail.com>
2022-05-31 23:20:43 +0530

    todo 4 and todo 5 done.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from twilio.rest import Client
 
 STOCK_NAME = "TSLA"
 COMPANY_NAME = "Tesla Inc"
@@ -7,6 +8,8 @@ STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
 MY_STOCK_API_KEY = "7DHYTNBBH9Q10EH5"
+NEWS_API_KEY = "4ae66f7d0c46452fb0ac8008bd89de04"
+TWILIO_SID = "ACfe05708775a4a7b91ee55303e6704b56"
 
     ## STEP 1: Use https://www.alphavantage.co/documentation/#daily
 # When stock price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
@@ -43,17 +46,27 @@ print(diff_percent)
 
 #. - If TODO4 percentage is greater than 5 then print("Get News").
 
-if diff_percent > 5:
-    print("Get news")
+if diff_percent > 1:
+    news_params = {
+        "apiKey": NEWS_API_KEY,
+        "qInTitle": COMPANY_NAME
+    }
 
+    new_response = requests.get(NEWS_ENDPOINT, params=news_params)
+    articles = new_response.json()["articles"]
+
+
+    three_articles = articles[:3]
     ## STEP 2: https://newsapi.org/ 
     # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
 
-#TODO 6. - Instead of printing ("Get News"), use the News API to get articles related to the COMPANY_NAME.
+ #Instead of printing ("Get News"), use the News API to get articles related to the COMPANY_NAME.
+
+
 
 #TODO 7. - Use Python slice operator to create a list that contains the first 3 articles. Hint: https://stackoverflow.com/questions/509211/understanding-slice-notation
 
-
+    [f"Headllines: {article ['title']}. \n Brief: {article ['description']}" for article in three_articles]
     ## STEP 3: Use twilio.com/docs/sms/quickstart/python
     #to send a separate message with each article's title and description to your phone number. 
 
